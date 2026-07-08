e631c7dcd7857f0e0a56a6ec6214bf7f16234e69
Malak <Malakkassas95@gmail.com>
2026-07-08 23:19:50 +0200

    tailwand

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// إنشاء عنصر nav
+// إنشاء nav
 const nav = document.createElement("nav");
+nav.className = "bg-blue-500 p-4 rounded-lg";
 
 // إنشاء ul
 const ul = document.createElement("ul");
+ul.className = "flex justify-center gap-8 text-white font-bold";
 
 // عناصر القائمة
 const menu = [
@@ -13,11 +15,14 @@ const menu = [
 ];
 
 // إنشاء li لكل عنصر
-menu.forEach(function(item){
+menu.forEach(function(item) {
 
     const li = document.createElement("li");
 
     li.textContent = item;
+
+    li.className =
+        "cursor-pointer hover:text-yellow-300 transition";
 
     ul.appendChild(li);
 
@@ -28,8 +33,9 @@ nav.appendChild(ul);
 
 // إضافة nav داخل الصفحة
 document.getElementById("menu").appendChild(nav);
-// DOM Task
 
+
+// DOM Task
 const text = document.getElementById("text");
 const btn = document.getElementById("btn");
 
@@ -38,3 +44,39 @@ btn.addEventListener("click", function () {
     text.style.color = "red";
     text.style.fontSize = "30px";
 });
+
+
+// BOM Task
+const alertBtn = document.getElementById("alertBtn");
+const promptBtn = document.getElementById("promptBtn");
+const confirmBtn = document.getElementById("confirmBtn");
+
+
+// Alert
+alertBtn.addEventListener("click", function () {
+    alert("Welcome to JavaScript BOM");
+});
+
+
+// Prompt
+promptBtn.addEventListener("click", function () {
+    const name = prompt("Enter your name:");
+
+    if (name) {
+        alert("Welcome " + name);
+    }
+});
+
+
+// Confirm
+confirmBtn.addEventListener("click", function () {
+    const result = confirm("Do you want to continue?");
+
+    if (result) {
+        alert("You clicked OK");
+    } else {
+        alert("You clicked Cancel");
+    }
+});
+
+
